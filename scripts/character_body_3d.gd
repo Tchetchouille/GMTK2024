0f53194e782e9e477c575d87095e7c3809042912
gaelborgeaud <gael.borgeaud@heig-vd.ch>
2024-08-17 03:40:47 +0200

    Added mouse movement

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const MOUSE_SENSITIVITY = 0.1
 
 var target_velocity = Vector3.ZERO
 
@@ -9,6 +10,8 @@ var target_velocity = Vector3.ZERO
 func _physics_process(delta):
 	
 	var direction = Vector3.ZERO
+	
+	
 	
 	if Input.is_action_pressed("move_forward") :
 		direction.z -= 1
@@ -19,16 +22,23 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_left") :
 		direction.x -= 1
 		
+	var movement_dir = transform.basis * Vector3(direction.x, 0, direction.z)
+		
 	if direction != Vector3.ZERO :
 		direction = direction.normalized()
 		
-	target_velocity.x = direction.x * SPEED
-	target_velocity.z = direction.z * SPEED
+	velocity.x = movement_dir.x * SPEED
+	velocity.z = movement_dir.z * SPEED
+		
+	#target_velocity.x = direction.x * SPEED
+	#target_velocity.z = direction.z * SPEED
 	
-	velocity = target_velocity
+	#velocity = target_velocity
 	
 	move_and_slide()
 	
-	
-	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+
 	
