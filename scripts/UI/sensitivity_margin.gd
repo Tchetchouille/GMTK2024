586927693222673d76f9c5f25ce6dd398e028ab7
Tchetchouille <jeremias.kuehne@protonmail.com>
2024-08-17 21:11:28 +0200

    SaveSystem added.

--- scripts/UI/sensitivity_margin.gd
+++ scripts/UI/sensitivity_margin.gd
@@ -1,6 +1,4 @@
 extends MarginContainer
 
-var character = preload("res://scenes/Character.tscn")
-
 func _on_sensitivity_slider_value_changed(value: float) -> void:
-	print(character)
+	SaveSystem.set_var("look_sensitivity", value * 0.01)
