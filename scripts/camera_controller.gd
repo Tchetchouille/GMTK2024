af1c12a818dfa3b3980c2f93f5fe29a6f84df813
Tchetchouille <jeremias.kuehne@protonmail.com>
2024-08-17 22:14:47 +0200

    Camera rework.

---
+++ scripts/camera_controller.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+@export var offset = 5;
+
+# Shamelessly stolen from: https://www.youtube.com/watch?v=yVde3I3K7oo
+func _process(delta: float) -> void:
+	var new_pos = Vector3($"..".position.x, position.y, $"..".position.z)
+	if position.distance_to(new_pos) >= offset:
+		position = lerp(position, new_pos, 0.01)
