4a422f591c7add6c83742589d00c5bc877c87473
rgborgeaud <gael.borgeaud@heig-vd.ch>
2024-08-17 17:27:39 +0200

    Added attack function

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -6,11 +6,25 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 var weapons_in_range = []
 var current_weapon_resource = null
-
+var is_picking_up = false
+var pickup_progress = 0
+var picking_up_weapon = null
 
 func _physics_process(delta):
 	
 	var direction = Vector3.ZERO
+	
+	if is_picking_up:
+		if Input.is_action_just_pressed("click_action") :
+			pickup_progress += 10
+			
+			if pickup_progress > 100:
+				current_weapon_resource = picking_up_weapon.pick_up()
+				pickup_progress = 0
+				picking_up_weapon = null
+				is_picking_up = false
+				print("Weapon picked up!")
+		return
 	
 	if Input.is_action_pressed("move_forward") :
 		direction.z -= 1
@@ -24,6 +38,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("click_action") :
 		if weapons_in_range :
 			pickup_weapon()
+		else :
+			attack()
 		
 	var movement_dir = transform.basis * Vector3(direction.x, 0, direction.z)
 		
@@ -43,15 +59,17 @@ func _unhandled_input(event):
 
 func _on_collision_area_body_entered(body):
 	print("Entered")
-	if body.get_parent().get_parent().get_parent() is Weapon :
+	var weapon = body.get_parent().get_parent()
+	if weapon is Weapon :
 		print("Entered weapon")
-		weapons_in_range.append(body.get_parent())
+		weapons_in_range.append(weapon)
 
 func _on_collision_area_body_exited(body):
 	print("Exited")
-	if body.get_parent().get_parent().get_parent() is Weapon :
+	var weapon = body.get_parent().get_parent()
+	if weapon is Weapon :
 		print("Exited weapon")
-		weapons_in_range.erase(body.get_parent())
+		weapons_in_range.erase(weapon)
 
 func pickup_weapon() :
 	var min_distance = INF
@@ -62,13 +80,16 @@ func pickup_weapon() :
 			min_distance = distance
 			closest_weapon = weapon
 	
-	var pickup_progress = 0
+	picking_up_weapon = closest_weapon
+	is_picking_up = true
 	
-	while(pickup_progress < 100) :
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) :
-			pickup_progress += 10
-		
-		
+func attack() :
+	var enemies = $AttackArea.get_overlapping_bodies()
+	print("Attack!")
+	for enemy in enemies :
+		if enemy.has_method("take_damage") :
+			enemy.take_damage()
+			print("Ouch") 
 	
-	current_weapon_resource = closest_weapon.get_parent().get_parent().pick_up()
+	
 	
