24b36bd0f25fee107fca04a60fb454f5e6ae530c
Tchetchouille <jeremias.kuehne@protonmail.com>
2024-08-17 15:06:22 +0200

    Fixed Back button issues.

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var settings_menu = preload("res://scenes/UI/Menus/settings_menu.tscn")
-
 # Loading the main scene when play button is pressed.
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
