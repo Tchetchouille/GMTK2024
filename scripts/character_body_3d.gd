e1d07842fc83813a5e6d4968b824b0a48130bc47
Florian Rieder <florian.rieder@unil.ch>
2024-08-18 15:22:39 +0200

    Kill to scale

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -71,7 +71,7 @@ func _physics_process(delta):
 		direction.x += 1
 	if Input.is_action_pressed("move_left") :
 		direction.x -= 1
-		
+
 	if Input.is_action_just_pressed("click_action") :
 		if weapons_in_range :
 			pickup_weapon()
